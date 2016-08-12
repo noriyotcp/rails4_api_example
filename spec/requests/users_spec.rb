83ef4d2cc14a94a8234f06d99ba891bc29fe723b
Noriyo Akita <noriyo.akita@gmail.com>
2016-08-12 12:21:56 +0000

    Add test for show and refactor

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -2,28 +2,44 @@ require 'rails_helper'
 
 RSpec.describe "Users", type: :request do
   describe "GET api/v1/users" do
-    before {
-      @users = FactoryGirl.create_list(:user, 2)
-      @saitamas = FactoryGirl.create_list(:user, 2, :saitama)
-    }
+    let!(:users) { FactoryGirl.create_list(:user, 2) }
+    let!(:saitamas) { FactoryGirl.create_list(:user, 2, :saitama) }
+
+    before { get api_v1_users_path(addr1: "Tokyo", format: :json) }
+
     it "works! (now write some real specs)" do
-      get api_v1_users_path(addr1: "Tokyo", format: :json)
       expect(response).to have_http_status(200)
     end
 
     it "should get json" do
-      get api_v1_users_path(addr1: "Tokyo", format: :json)
       json = JSON.parse(response.body)
-      expect(json.size).to eq @users.count
-      expect(json[0]["id"]).to eq @users[0].id
-      expect(json[1]["id"]).to eq @users[1].id
+      expect(json.size).to eq users.count
+      expect(json[0]["id"]).to eq users[0].id
+      expect(json[1]["id"]).to eq users[1].id
     end
 
     it "should not get json of saitama" do
-      get api_v1_users_path(addr1: "Tokyo", format: :json)
       json = JSON.parse(response.body)
-      expect(json[0]["id"]).not_to eq @saitamas[0].id
-      expect(json[1]["id"]).not_to eq @saitamas[1].id
+      expect(json[0]["id"]).not_to eq saitamas[0].id
+      expect(json[1]["id"]).not_to eq saitamas[1].id
+    end
+  end
+
+  describe "GET api/v1/users/:id" do
+    let(:user) { FactoryGirl.create(:user) }
+
+    before { get api_v1_user_path(user.id) }
+
+    it "should be success" do
+      expect(response).to have_http_status(200)
+    end
+
+    it "should get json" do
+      json = JSON.parse(response.body)
+      expect(json["id"]).to eq user.id
+      expect(json["name"]).to eq user.name
+      expect(json["addr1"]).to eq user.addr1
+      expect(json["addr2"]).to eq user.addr2
     end
   end
 end
