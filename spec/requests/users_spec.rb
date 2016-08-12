49ade2549d6d52c7b7c3e38b81249c8eac0c4be4
Noriyo Akita <noriyo.akita@gmail.com>
2016-08-12 09:57:07 +0000

    Add test for index

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -1,10 +1,29 @@
 require 'rails_helper'
 
 RSpec.describe "Users", type: :request do
-  describe "GET /users" do
+  describe "GET api/v1/users" do
+    before {
+      @users = FactoryGirl.create_list(:user, 2)
+      @saitamas = FactoryGirl.create_list(:user, 2, :saitama)
+    }
     it "works! (now write some real specs)" do
-      get users_path
+      get api_v1_users_path(addr1: "Tokyo", format: :json)
       expect(response).to have_http_status(200)
+    end
+
+    it "should get json" do
+      get api_v1_users_path(addr1: "Tokyo", format: :json)
+      json = JSON.parse(response.body)
+      expect(json.size).to eq @users.count
+      expect(json[0]["id"]).to eq @users[0].id
+      expect(json[1]["id"]).to eq @users[1].id
+    end
+
+    it "should not get json of saitama" do
+      get api_v1_users_path(addr1: "Tokyo", format: :json)
+      json = JSON.parse(response.body)
+      expect(json[0]["id"]).not_to eq @saitamas[0].id
+      expect(json[1]["id"]).not_to eq @saitamas[1].id
     end
   end
 end
