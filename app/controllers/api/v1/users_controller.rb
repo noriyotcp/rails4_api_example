1d5ef61ece7303e966d5355672a0ab329b1981b7
Noriyo Akita <noriyo.akita@gmail.com>
2016-08-13 09:52:41 +0000

    Remove useless lines

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -4,20 +4,8 @@ class Api::V1::UsersController < ApplicationController
     @users = User.name_like(params[:name])
                  .by_addr1(params[:addr1])
                  .addr2_like(params[:addr2])
-    # not use jbuilder
-    # @users = @users.map do |user|
-    #   {
-    #     id: user.id,
-    #     name: user.name,
-    #     address: {
-    #       addr1: user.addr1,
-    #       addr2: user.addr2
-    #     }
-    #   }
-    # end
-    # render json: @users
     respond_to do |format|
-      format.html # index.html.erb
+      # format.html # index.html.erb
       format.json # index.json.jbuilder
     end
   end
