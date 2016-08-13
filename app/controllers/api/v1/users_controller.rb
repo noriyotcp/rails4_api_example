87c04d476cd302d07e8bb8cdb0f61a5942d9ba76
Noriyo Akita <noriyo.akita@gmail.com>
2016-08-13 09:52:29 +0000

    Add create action

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -26,6 +26,16 @@ class Api::V1::UsersController < ApplicationController
     @user = User.find(params[:id])
   end
 
+  def create
+    @user = User.new(user_params)
+
+    if @user.save
+      render :show, status: :created, location: [:api, :v1, @user]
+    else
+      render json: @user.errors, status: :unprocessable_entity
+    end
+  end
+
   private
 
   def user_params
